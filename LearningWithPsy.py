4c8c1c09aa1b976284d5fc98e424da93357057c6
VidaTurtles <purvis.jackson@yahoo.com>
2014-11-26 05:11:34 -0500

    First Terribleness
    :/

---
+++ LearningWithPsy.py
@@ -0,0 +1,18 @@
+__author__ = 'Turtle'
+print("Hi I'm new to this")
+print("I'm going to try to make the number 10 turn into the number 5")
+var1 = 10
+print(var1)
+var2 = 5
+print(var2)
+print("So We'll take var1 and subtract var2 from it!")
+var1 - var2
+print(var1 - var2)
+print("Now that we did that let's try to make it turn into 15!")
+var3 = 10
+print(var3)
+var4 = 5
+print(var4)
+print('So we have var3 and var4 now')
+print(var3+var4)
+print(gettime)
